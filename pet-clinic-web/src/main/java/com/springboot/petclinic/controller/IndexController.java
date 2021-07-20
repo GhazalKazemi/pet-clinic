42e4b47e799a3aa62e44d3dde1bcac67568064bc
Ghazal <ghazal.kazemi@gmail.com>
2021-07-20 11:25:17 +1000

    Add controller and index page. closes #8

---
+++ pet-clinic-web/src/main/java/com/springboot/petclinic/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.springboot.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+    @GetMapping({"/","","index","index.html"})
+    public String showHomepage(){
+        return "index";
+    }
+}
