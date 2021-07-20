98b90699226f871fe02a978cf50ca4633dc5f0aa
Ghazal <ghazal.kazemi@gmail.com>
2021-07-20 12:34:48 +1000

    Implement Owner Controller and view. Closes #13

---
+++ pet-clinic-web/src/main/java/com/springboot/petclinic/controller/OwnerController.java
@@ -0,0 +1,14 @@
+package com.springboot.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+    @GetMapping({"", "/", "/index"})
+    public String showOwners(){
+        return "owner/index";
+    }
+}
