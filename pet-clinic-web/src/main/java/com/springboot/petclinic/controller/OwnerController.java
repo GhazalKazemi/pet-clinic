0f996d29a5bd7d7a712b2403cac6ff08d79e618e
Ghazal <ghazal.kazemi@gmail.com>
2021-07-21 10:25:09 +1000

    Add model and view for Owner. closes #16

--- pet-clinic-web/src/main/java/com/springboot/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springboot/petclinic/controller/OwnerController.java
@@ -1,14 +1,28 @@
 package com.springboot.petclinic.controller;
 
+import com.springboot.petclinic.entity.Owner;
+import com.springboot.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"", "/", "/index"})
-    public String showOwners(){
+    public String showOwners(Model model){
+        Set<Owner> owners = ownerService.findAll();
+        model.addAttribute("owners", owners);
         return "owner/index";
     }
 }
