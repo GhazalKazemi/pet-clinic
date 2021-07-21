505a1fc837f869c471e2d754b4423e96a4d281d8
Ghazal <ghazal.kazemi@gmail.com>
2021-07-21 10:31:54 +1000

    Add model and view for Vet. closes #15

--- pet-clinic-web/src/main/java/com/springboot/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/springboot/petclinic/controller/VetController.java
@@ -1,15 +1,23 @@
 package com.springboot.petclinic.controller;
 
+import com.springboot.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @GetMapping({"/vets","/vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
