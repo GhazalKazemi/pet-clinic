07e40a7d4bcb51d89a9914e8427b696e7dd894aa
Ghazal <ghazal.kazemi@gmail.com>
2021-07-23 10:28:39 +1000

    Implement PetType Map Service. Closes #29

---
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/map/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package com.springboot.petclinic.map;
+
+import com.springboot.petclinic.entity.Pet;
+import com.springboot.petclinic.entity.PetType;
+import com.springboot.petclinic.service.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.springboot.petclinic.service;
+
+import com.springboot.petclinic.entity.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
