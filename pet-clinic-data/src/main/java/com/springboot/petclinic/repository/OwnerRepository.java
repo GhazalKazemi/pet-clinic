539df8fd742f1c39c98d3d7f8d23061b71f975a2
Ghazal <ghazal.kazemi@gmail.com>
2021-07-26 19:45:13 +1000

    Implement Owners JPA service. Closes #40

--- pet-clinic-data/src/main/java/com/springboot/petclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/repository/OwnerRepository.java
@@ -4,4 +4,5 @@ import com.springboot.petclinic.entity.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/service/OwnerServiceImpl.java
@@ -0,0 +1,49 @@
+package com.springboot.petclinic.service;
+
+import com.springboot.petclinic.entity.Owner;
+import com.springboot.petclinic.repository.OwnerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+@Service
+public class OwnerServiceImpl implements OwnerService{
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerServiceImpl(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+}
