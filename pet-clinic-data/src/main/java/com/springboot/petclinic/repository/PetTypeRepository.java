00e627d2c773dd82e7580e7bb7fcdecd29e822a9
Ghazal <ghazal.kazemi@gmail.com>
2021-07-27 13:23:28 +1000

    Implement PetType data JPA service. Closes #42

--- pet-clinic-data/src/main/java/com/springboot/petclinic/repository/PetTypeRepository.java
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/repository/PetTypeRepository.java
@@ -1,7 +1,8 @@
 package com.springboot.petclinic.repository;
 
 
+import com.springboot.petclinic.entity.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetTypeRepository extends CrudRepository<PetTypeRepository, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
---
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/service/PetTypeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.springboot.petclinic.service;
+
+import com.springboot.petclinic.entity.PetType;
+import com.springboot.petclinic.repository.PetTypeRepository;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("spring-data-jpa")
+public class PetTypeServiceImpl implements PetTypeService{
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceImpl(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+}
