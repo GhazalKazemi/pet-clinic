ce8e13cd665e1526d6ba36e142fe391977897cab
Ghazal <ghazal.kazemi@gmail.com>
2021-07-28 15:25:42 +1000

    Add project Lombok. Closes #51

--- pet-clinic-data/src/main/java/com/springboot/petclinic/entity/Owner.java
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/entity/Owner.java
@@ -1,8 +1,13 @@
 package com.springboot.petclinic.entity;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
@@ -15,35 +20,12 @@ public class Owner extends Person{
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
+    @Builder
+    public Owner(String firstName, String lastName, String address, String city, String phoneNumber, Set<Pet> pets) {
+        super(firstName, lastName);
         this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
 }
