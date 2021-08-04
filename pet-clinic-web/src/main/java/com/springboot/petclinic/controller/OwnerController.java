ca97d3ba9d827989d0eccf4182197a82c57c0f63
Ghazal <ghazal.kazemi@gmail.com>
2021-08-04 10:24:06 +1000

    Implement Display Owner (TDD). Closes #53

--- pet-clinic-web/src/main/java/com/springboot/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springboot/petclinic/controller/OwnerController.java
@@ -1,11 +1,14 @@
 package com.springboot.petclinic.controller;
 
 import com.springboot.petclinic.entity.Owner;
+import com.springboot.petclinic.entity.Pet;
 import com.springboot.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Set;
 
@@ -28,5 +31,14 @@ public class OwnerController {
     @GetMapping("/find")
     public String findOwners(){
         return "not-implemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView modelAndView = new ModelAndView("owner/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+
+        modelAndView.addObject(owner);
+        return modelAndView;
     }
 }
