65cc534078ee0a2fbe86c82118dd1936dba8af39
Ghazal <ghazal.kazemi@gmail.com>
2021-08-09 11:34:46 +1000

    Convert LocalDate format and NullPointerException bug. Closes #79

--- pet-clinic-data/src/main/java/com/springboot/petclinic/entity/Pet.java
+++ pet-clinic-data/src/main/java/com/springboot/petclinic/entity/Pet.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -24,7 +25,7 @@ public class Pet extends BaseEntity {
     @Column(name = "birth_date")
     private LocalDate birthDate;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
-    private Set<Visit> visits;
+    private Set<Visit> visits = new HashSet<>();
 
     @Builder
     public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
